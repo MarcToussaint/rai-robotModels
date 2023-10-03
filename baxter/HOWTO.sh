df61beba583734e1110af6a11b41e0a9e3d1015f
Marc Toussaint <toussaint@tu-berlin.de>
2023-10-03 12:05:12 +0200

    baxter redone

--- baxter/HOWTO.sh
+++ baxter/HOWTO.sh
@@ -1,8 +1,6 @@
 #rosrun xacro xacro --inorder `rospack find baxter_description`/urdf/baxter.urdf.xacro > baxter2.urdf
 urdf2rai.py baxter2.urdf > z.1.g
-sed 's/baxter_description\/meshes\///g' z.1.g > z.2.g
-sed 's/rethink_ee_description\/meshes\///g' z.2.g > z.3.g
-sed 's/\.DAE/.ply/g' z.3.g > z.4.g
+sed 's/\.DAE/.ply/g' z.1.g > z.2.g
 
-kinEdit -file z.4.g -cleanOnly
+kinEdit -file z.2.g -cleanOnly
 mv z.g baxter_clean2.g
