ee04427d08d7b46f238f6c7d7a058fc412dfa381
Marc Toussaint <toussaint@tu-berlin.de>
2026-07-19 09:38:15 +0200

    include reduced cvx stl meshes

---
+++ g1/process_meshes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from robotic.src.mesh_tool import MeshTool
+from pathlib import PurePath, Path
+import os
+import glob
+
+path = 'meshes/'
+
+files = sorted(glob.glob(path+'*.h5'))
+
+for file in files:
+    
+    ### load
+    mesh = MeshTool(file)
+    if mesh==None:
+        continue
+
+    # mesh.export_trimesh(ext='-.ply')
+    
+    ### meshlab repair
+    mesh.report()
+
+    ### trimesh repair
+    mesh.repair_meshlab(merge_threshold=.005, make_convex=True)
+    mesh.report()
+
+    # mesh.view()
+
+    ### export .stl
+    filebase = os.path.splitext(file)[0]
+    name = PurePath(filebase).name
+    mesh.export_trimesh(ext='.stl')
+
+    print('=== done: ', file)
+
+
